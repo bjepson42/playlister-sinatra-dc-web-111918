0ba7f1ff194419376eadfc4be52218a481deb627
github username <github email address>
2018-12-12 18:50:07 -0500

    finished index

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,17 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :artists, through: :songs
   has_many :songs, through: :song_genres
+
+  def slug
+  	name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+  	Genre.all.select do |genre|
+  		if genre.slug == slug
+  			return genre
+  		end
+  	end
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :genres, through: :song_genres
 
+  def slug
+  	name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+  	Song.all.select do |song|
+  		if song.slug == slug
+  			return song
+  		end
+  	end
+  end
 end
