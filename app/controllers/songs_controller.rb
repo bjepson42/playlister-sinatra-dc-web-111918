98cb3a4e12b57fef81f2a2b71b80c91b1b2c7a3a
github username <github email address>
2018-12-13 14:34:32 -0500

    add new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,11 +6,23 @@ class SongsController < ApplicationController
 		erb :'/songs/index'
 	end
 
+	get '/songs/new' do
+		@songs = Song.new
+		erb :'/songs/new'
+	end
+
+	post'/songs' do
+		@song = Song.create(params)
+		flash[:message] = "Successfully created song."
+		redirect "/songs/#{@song.slug}"
+	end
+
 	get '/songs/:slug' do
 		slug = params[:slug]
 		@song = Song.find_by_slug(slug)
 		erb :'/songs/show'
-
 	end
+
+	
 
 end
